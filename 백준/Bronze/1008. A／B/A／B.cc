aa0ffa025874c741c2819dc3df4570bf3b029b6f
hongsungil12345678 <123550360+hongsungil12345678@users.noreply.github.com>
2023-02-15 23:58:44 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.cc
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -1,10 +1,10 @@
 #include<iostream>
+#include<stdio.h>
 using namespace std;
 int main(){
     double a,b;
     cin>>a>>b;
-    cout<<fixed;
-    cout.precision(9);
-    cout<<a/b;
+    printf("%.9f",a/b);
     return 0;
+    
 }
