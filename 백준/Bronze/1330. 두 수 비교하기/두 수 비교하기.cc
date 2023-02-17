b37658de4b1e531056549b9ea64d4723cb79901d
hongsungil12345678 <123550360+hongsungil12345678@users.noreply.github.com>
2023-02-18 01:31:35 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,16 @@
+#include<stdio.h>
+using namespace std;
+int main(){
+    int a,b;
+    scanf("%d%d",&a,&b);
+    if(a>b){
+        printf(">");
+    }
+    else if(b>a){
+        printf("<");
+    }
+    else if(a==b){
+        printf("==");
+    }
+    return 0;
+}
