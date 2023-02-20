dc76d5399c6c6d3a4fc4c2ff07ec9a58c1802330
hongsungil12345678 <123550360+hongsungil12345678@users.noreply.github.com>
2023-02-21 00:07:03 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 4 MB -BaekjoonHub

--- 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.cpp
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.cpp
@@ -8,8 +8,6 @@ vector<int> solution(vector<int> array) {
     vector<int> answer;
     int max=*max_element(array.begin(),array.end());
     int temp=max_element(array.begin(),array.end())-array.begin();
-    cout<<max<<endl;
-    cout<<temp<<endl;
     answer.push_back(max);
     answer.push_back(temp);
     return answer;
