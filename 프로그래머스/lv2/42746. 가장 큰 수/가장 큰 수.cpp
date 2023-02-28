bfaafac7b49bf44b5e6d20649612a53ec10edf60
hongsungil12345678 <123550360+hongsungil12345678@users.noreply.github.com>
2023-03-01 01:37:33 +0900

    [level 2] Title: 가장 큰 수, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.cpp
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -2,22 +2,22 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-bool compare(string a,string b){// a+b b+a 둘중에 뭐가 더 큰지
-    return a+b>b+a;// 큰수가 나오게 반환
+bool compare(string a,string b){
+    return a+b>b+a;//큰수 반환
 }
+
 string solution(vector<int> numbers) {
     string answer = "";
     vector<string> temp;
-    for(auto num:numbers){// int -> string 변환 저장
-        temp.push_back(to_string(num));
+    for(auto num:numbers){
+        temp.push_back(to_string(num));// int -> string 저장
     }
-    sort(temp.begin(),temp.end(),compare);// 정렬
+    sort(temp.begin(),temp.end(),compare);//string 정렬
     if(temp.at(0)=="0"){
         return "0";
     }
-    for(auto str:temp){
-        answer+=str;
+    for(auto tmp:temp){
+        answer+=tmp;
     }
-    
     return answer;
 }
