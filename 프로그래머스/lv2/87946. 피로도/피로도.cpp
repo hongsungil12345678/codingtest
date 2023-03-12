d6b4390f64224e6ccdc26c73d3821a608d7ada22
hongsungil12345678 <123550360+hongsungil12345678@users.noreply.github.com>
2023-03-12 18:31:13 +0900

    [level 2] Title: 피로도, Time: 0.01 ms, Memory: 4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+bool visted[9]={false};
+int answer = -1;
+vector<vector<int>> dun;
+void dfs(int k,int count){
+    if(k<0){
+        return;
+    }
+    for(int i=0;i<dun.size();i++){
+        if(!visted[i]&&k>=dun[i][0]){
+            visted[i]=true;
+            dfs(k-dun[i][1],count+1);
+            visted[i]=false;
+        }
+    }
+    answer=max(answer,count);
+}
+int solution(int k, vector<vector<int>> dungeons) {
+    dun=dungeons;
+    dfs(k,0);
+    return answer;
+}
