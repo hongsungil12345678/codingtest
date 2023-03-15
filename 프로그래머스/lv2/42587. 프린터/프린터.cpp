2032f51d5b0c46a0126e3d8d232d8db75df9b33b
hongsungil12345678 <123550360+hongsungil12345678@users.noreply.github.com>
2023-03-15 20:25:26 +0900

    [level 2] Title: 프린터, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프린터/프린터.cpp
+++ 프로그래머스/lv2/42587. 프린터/프린터.cpp
@@ -5,21 +5,22 @@ using namespace std;
 
 int solution(vector<int> priorities, int location) {
     int answer = 0;
-    int count =0;
-    queue<pair<int,int>>temp;
-    priority_queue<int> pri;
+    int count=0;
+    queue<pair<int,int>> temp;
+    priority_queue <int> pri;
     for(int i=0;i<priorities.size();i++){
         temp.push(make_pair(i,priorities[i]));
         pri.push(priorities[i]);
     }
+    
     while(!temp.empty()){
-        int index = temp.front().first;
-        int value = temp.front().second;
+        int index=temp.front().first;
+        int value=temp.front().second;
         temp.pop();
         if(pri.top()==value){
             pri.pop();
             count++;
-            if(index==location){
+            if(location==index){
                 answer=count;
                 break;
             }
@@ -28,9 +29,5 @@ int solution(vector<int> priorities, int location) {
             temp.push(make_pair(index,value));
         }
     }
-    
-    
-    
-    
     return answer;
 }
