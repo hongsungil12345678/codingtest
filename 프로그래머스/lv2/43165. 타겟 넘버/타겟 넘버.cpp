a0a5ee0d2bdd6d78003d1434aea4354707e27c68
hongsungil12345678 <123550360+hongsungil12345678@users.noreply.github.com>
2023-03-22 17:06:38 +0900

    [level 2] Title: 타겟 넘버, Time: 0.92 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -4,16 +4,18 @@
 using namespace std;
 int answer=0;
 void targetNumber(vector<int>numbers,int target,int sum,int index){
-    if(index==numbers.size()){
+    if(numbers.size()==index){
         if(target==sum){
             answer++;
         }
         return;
     }
+    // + , - 재귀
     targetNumber(numbers,target,sum+numbers[index],index+1);
     targetNumber(numbers,target,sum-numbers[index],index+1);
-    
 }
+
+
 int solution(vector<int> numbers, int target) {
     targetNumber(numbers,target,0,0);
     return answer;
