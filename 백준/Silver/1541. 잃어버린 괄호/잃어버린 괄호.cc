9623b2544d580245f1bc254522b1b03ce3bb446c
hongsungil12345678 <123550360+hongsungil12345678@users.noreply.github.com>
2024-04-09 16:15:09 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
@@ -1,35 +1,31 @@
 #include<iostream>
 #include<string>
 using namespace std;
-
 string str;
-int main(void) {
-	string num = "";		//	숫자를 저장할 문자열 변수
-	bool minus=false;		//	'-' 기호가 나왔는지를 확인하는 변수
-	int sum = 0;		//	출력하게될 결과
-	cin >> str;
-    
-	for (int i = 0; i <= str.size(); i++) {
-		if (str[i] == '+' || str[i] == '-' || str[i] == '\0') {	
-        	//	기호일 경우
-			if (minus == true) {
-            		//	'-' 기호가 한번이라도 나왔다면 그 뒤의 숫자들은 모두 빼주면 된다.
-				sum -= stoi(num);
-				num = "";		// 초기화
-			}
-			else if (minus == false) {
-            		//	반대로 '-' 기호가 아직 나오지 않았다면, 나오기 전까지는 모두 더해주면 된다.
-				sum += stoi(num);
-				num = "";		
-			}
-		}
-		else {		//	숫자일 경우
-			num += str[i];
-		}
-        if(str[i]=='-'){
-            minus=true;
+int main(void){
+    string num = ""; // 임시 문자열 저장
+    int sum = 0; // 정답
+    bool isMinus = false; // 기호 판단
+    cin>>str;
+    for(int i=0;i<=str.size();i++){
+        // + , - , 문자열 마지막(\0)NULL 이거나
+        if(str[i]=='-'|| str[i]=='+' || str[i]=='\0'){
+            if(isMinus == true){ // 문자열이 - 인경우
+                sum -= stoi(num);  // 빼준다
+                num = ""; // 초기화
+            }
+            else if(isMinus == false){ // + 인경우
+                sum += stoi(num);
+                num = "";
+            }
         }
-	}
-	cout << sum;
-	return 0;
+        else{ // 기호가 아닌경우 문자열 저장
+            num += str[i];
+        }
+        if(str[i]=='-'){ // 기호인 경우
+            isMinus = true;
+        }
+    }
+    cout<<sum;
+    return 0;
 }
